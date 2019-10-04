3411b0fb3b25219e89b1bd24510ac5e1bc46b088
Amul Karuvath <akaruvath@highroads.com>
2019-10-04 23:58:11 +0530

    implemented cart items and incrementing quantity

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,20 +1,27 @@
 import CartActionTypes from "./cart.types";
 
+import {addItemToCart} from "./cart.utils"
+
 const INITIAL_STATE = {
-  hidden: true
-}
+  hidden: true,
+  cartItems: []
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
-      console.log(state.hidden);
       return {
         ...state,
         hidden: !state.hidden
       };
+    case CartActionTypes.ADD_ITEM: ;
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
     default:
       return action;
   }
-}
+};
 
 export default cartReducer;
