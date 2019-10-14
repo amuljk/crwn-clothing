8cd340f0f7183adf1b205b562fcbec3550d376f6
Amul Karuvath <akaruvath@highroads.com>
2019-10-14 13:25:11 +0530

    Implemented remove from cart feature

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,18 +1,35 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-
-  if(!cartItems) {
-    return [{...cartItemToAdd, quantity: 1}];
+  if (!cartItems) {
+    return [{ ...cartItemToAdd, quantity: 1 }];
   }
 
-  let existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-  
-  if(existingCartItem) {
-    return cartItems.map(cartItem => 
-      cartItem.id === cartItemToAdd.id 
-      ? {...cartItem, quantity: cartItem.quantity + 1 }
-      : cartItem
+  let existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
   }
 
-  return [...cartItems, {...cartItemToAdd, quantity: 1}]
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  let existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
